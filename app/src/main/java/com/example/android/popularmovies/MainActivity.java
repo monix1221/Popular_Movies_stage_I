00ae2a97ff422ebe4d288c774477ecc953efd967
Monika Antoń <monikax1221@gmail.com>
2017-05-10 11:28:59 +0200

    loader added successfully

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.android.popularmovies;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.os.AsyncTask;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -21,8 +23,7 @@ import com.example.android.popularmovies.utilities.NetworkUtils;
 import java.net.URL;
 import java.util.ArrayList;
 
-
-public class MainActivity extends AppCompatActivity implements ImageAdapter.ImageAdapterOnClickHandler{
+public class MainActivity extends AppCompatActivity implements ImageAdapter.ImageAdapterOnClickHandler, LoaderManager.LoaderCallbacks<ArrayList<ItemModel>>{
 
     //needed only for intent
     public static final String MOVIE_TITLE = "movie_title";
@@ -41,12 +42,11 @@ public class MainActivity extends AppCompatActivity implements ImageAdapter.Imag
     MoviePreferences mSortOrderState=null;
     private static final String SELECTED_MOVIE_ORDER = "SelectedMovieOrder";
 
-    //needed for loader:
-    //a constant int to uniquely identify loader
     /*
-     * This number will uniquely identify our Loader and is chosen arbitrarily. You can change this
-     * to any number you like
+     * This number will uniquely identify Loader and is chosen arbitrarily.
+     * It can be any number
      */
+    private static final int MOVIE_SEARCH_LOADER = 22;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,12 +65,13 @@ public class MainActivity extends AppCompatActivity implements ImageAdapter.Imag
             mSortOrderState=savedInstanceState.getParcelable(SELECTED_MOVIE_ORDER);
         }else{
             mSortOrderState=MoviePreferences.POPULAR;
+            loadMovieData(mSortOrderState);
         }
-
-        loadMovieData(mSortOrderState);
 
         setSupportActionBar(binding.toolbar);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        getSupportLoaderManager().initLoader(MOVIE_SEARCH_LOADER, null, this);
     }
 
     @Override
@@ -81,12 +82,27 @@ public class MainActivity extends AppCompatActivity implements ImageAdapter.Imag
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        loadMovieData(mSortOrderState);
         super.onRestoreInstanceState(savedInstanceState);
     }
 
-    private void loadMovieData(MoviePreferences preferences) {
-        new MovieQueryTask().execute(preferences);
+    private void loadMovieData(MoviePreferences sortOrderPreference) {
+        sortOrderPreference = mSortOrderState;
+
+        Bundle queryBundle = new Bundle();
+        LoaderManager loaderManager = getSupportLoaderManager();
+        // Getting Loader by calling getLoader and passing the ID we specified
+        Loader<String> movieDataLoader = loaderManager.getLoader(MOVIE_SEARCH_LOADER);
+        // If the Loader was null, initializing it. Else, restarting it.
+        if (movieDataLoader == null) {
+            loaderManager.initLoader(MOVIE_SEARCH_LOADER, queryBundle, this);
+            mImageAdapter.setMovieData(null);
+            loadMovieData(mSortOrderState);
+
+        } else {
+            queryBundle.putParcelable(SELECTED_MOVIE_ORDER, sortOrderPreference);
+            loaderManager.restartLoader(MOVIE_SEARCH_LOADER, queryBundle, this);
+        }
+
     }
 
     private void setListToTopRated() {
@@ -102,7 +118,6 @@ public class MainActivity extends AppCompatActivity implements ImageAdapter.Imag
     private void showMovieDataGridView() {
         binding.tvErrorMessageDisplay.setVisibility(View.INVISIBLE);
         binding.rvImages.setVisibility(View.VISIBLE);
-
     }
 
     private void showErrorMessage() {
@@ -126,7 +141,6 @@ public class MainActivity extends AppCompatActivity implements ImageAdapter.Imag
             case R.id.action_refresh:
                 mImageAdapter.setMovieData(null);
                 loadMovieData(mSortOrderState);
-                //setListToPopular();
                 return true;
 
             case R.id.sort_by_popular:
@@ -134,22 +148,21 @@ public class MainActivity extends AppCompatActivity implements ImageAdapter.Imag
                 else item.setChecked(true);
 
                 mImageAdapter.setMovieData(null);
-
-                setListToPopular();
-                //
                 mSortOrderState=MoviePreferences.POPULAR;
+                setListToPopular();
+
                 String textToShow = getString(R.string.sorted_popular);
                 Toast.makeText(MainActivity.this, textToShow, Toast.LENGTH_SHORT).show();
-                return true; //must say true here!
+                return true;
 
             case R.id.sort_by_top_rated:
                 mImageAdapter.setMovieData(null);
                 if (item.isChecked()) item.setChecked(false);
                 else item.setChecked(true);
 
-                setListToTopRated();
-                //
                 mSortOrderState=MoviePreferences.TOP_RATED;
+                setListToTopRated();
+
                 String textToShow2 = getString(R.string.sorted_top_rated);
                 Toast.makeText(MainActivity.this, textToShow2, Toast.LENGTH_SHORT).show();
                 return true;
@@ -171,50 +184,88 @@ public class MainActivity extends AppCompatActivity implements ImageAdapter.Imag
         startActivity(intentToStartDetailActivity);
     }
 
-    //Inner class to handle background task
-    public class MovieQueryTask extends AsyncTask<MoviePreferences, Void, ArrayList<ItemModel>> {
+    @Override
+    public Loader<ArrayList<ItemModel>> onCreateLoader(int id, final Bundle args) {
+        //Return a new AsyncTaskLoader<String> as an anonymous inner class with this as the constructor's parameter
+        return new AsyncTaskLoader<ArrayList<ItemModel>>(this) {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            binding.pbLoadingIndicator.setVisibility(View.VISIBLE);
-        }
+            //needed for caching the results (we won't forceLoad again if we already got the results)
+            ArrayList<ItemModel> mMovieResults;
 
-        @Override
-        protected ArrayList<ItemModel> doInBackground(MoviePreferences... preferredSortOrder) {
-            if (preferredSortOrder.length == 0) {
+            @Override
+            protected void onStartLoading() {
+                // If args is null, return.
+                /* If no arguments were passed, we don't have a query to perform. Simply return. */
+                if (args == null) {
+                    return;
+                }
+
+                /*
+                 * When we initially begin loading in the background, we want to display the
+                 * loading indicator to the user
+                 */
+                binding.pbLoadingIndicator.setVisibility(View.VISIBLE);
+
+                if (mMovieResults != null){
+                    deliverResult(mMovieResults);
+                }else {
+                    forceLoad();
+                }
+            }
+
+            @Override
+            public ArrayList<ItemModel> loadInBackground() {
+
+                // Get the String for our URL from the bundle passed to onCreateLoader
+                MoviePreferences searchQueryUrlString = args.getParcelable(SELECTED_MOVIE_ORDER);
+
+                if (searchQueryUrlString == null) {
+                    mSortOrderState=MoviePreferences.POPULAR;
+                }
+
+                /* Parse the URL from the passed in String and perform the search */
+                String movieSearchResults = null;
+                try {
+                    URL searchUrl = NetworkUtils.buildUrl(searchQueryUrlString);
+                    movieSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+
+                    ArrayList<ItemModel> simpleJsonMovieData =
+                            MovieJsonUtils.getSimpleMovieStringsFromJson(movieSearchResults);
+
+                    return simpleJsonMovieData;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 return null;
             }
-            MoviePreferences sortOrderPreference = preferredSortOrder[0];
 
-            URL searchUrl = NetworkUtils.buildUrl(sortOrderPreference);
-            String movieSearchResults = null;
-            try {
-                movieSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
-
-                ArrayList<ItemModel> simpleJsonMovieData =
-                        MovieJsonUtils.getSimpleMovieStringsFromJson(movieSearchResults);
-
-                return simpleJsonMovieData;
-
-            } catch (Exception e) {
-                e.printStackTrace();
+            @Override
+            public void deliverResult(ArrayList<ItemModel> data) {
+                mMovieResults=data;
+                super.deliverResult(data);
             }
-            return null;
-        }
+        };
+    }
 
-        @Override
-        protected void onPostExecute(ArrayList<ItemModel> movieResults) {
-            binding.pbLoadingIndicator.setVisibility(View.INVISIBLE);
-            if (movieResults != null) {
-                mImageAdapter.setMovieData(movieResults);
-                showMovieDataGridView();
+    @Override
+    public void onLoadFinished(Loader<ArrayList<ItemModel>> loader, ArrayList<ItemModel> movieResults) {
+        binding.pbLoadingIndicator.setVisibility(View.INVISIBLE);
 
-            } else {
-                showErrorMessage();
-            }
+        /*
+         * If the results are null, we assume an error has occurred. There are much more robust
+         * methods for checking errors, but we wanted to keep this particular example simple.
+         */
+        if (null == movieResults) {
+            showErrorMessage();
+        } else {
+            mImageAdapter.setMovieData(movieResults);
+            showMovieDataGridView();
         }
     }
 
-
+    @Override
+    public void onLoaderReset(Loader<ArrayList<ItemModel>> loader) {
+        //not using this method here, but we are required to override it here
+    }
 }
